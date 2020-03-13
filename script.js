9f0293274ac6714e7eaf4c53f3260cf09108faf8
Dima Dubovski <dzmitry.duboyski@gmail.com>
2020-03-13 14:01:17 +0300

    feat: add action for block portfolio

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
 const menuNavigationClick =(elem)=>{
-document.querySelectorAll('.navigation__link').forEach(a => a.classList.remove('active'));
-elem.target.classList.add('active');
-
-
+  document.querySelectorAll('.navigation__link').forEach(el => el.classList.remove('active'));
+  elem.target.classList.add('active');
 }
 
-document.querySelector('.navigation').addEventListener('click',menuNavigationClick);
+const portfolioTagClick=(elem)=>{
+  console.log(elem)
+  document.querySelectorAll('.tag').forEach(el => el.classList.remove('active'));
+  elem.target.classList.add('active');
+  
+  let img= document.querySelector('.portfolio__example');
+  document.querySelector('.portfolio__example').remove();
+  document.querySelector('.portfolio__examples').insertAdjacentElement('beforeend',img);
+}
+
+const portfolioExamplesClick =(elem)=>{
+  document.querySelectorAll('.portfolio__image').forEach(el => el.classList.remove('active'));
+  elem.target.classList.add('active');
+}
+
+
+document.querySelector('.navigation').addEventListener('click', menuNavigationClick);
+document.querySelector('.portfolio__tags').addEventListener('click', portfolioTagClick);
+document.querySelector('.portfolio__examples').addEventListener('click', portfolioExamplesClick);
